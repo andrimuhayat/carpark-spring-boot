3ed41b72667baf01fa1322ee951ac79efefed6be
Andri Muhayat <muhayatandri@gmail.com>
2022-09-05 12:58:00 +0700

    add : fetch car availability when system startup

--- src/main/java/com/andri/carpark/config/AppStartup.java
+++ src/main/java/com/andri/carpark/config/AppStartup.java
@@ -15,6 +15,7 @@ public class AppStartup implements ApplicationListener<ApplicationReadyEvent> {
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
 		System.out.println("On startup");
+		carParkService.fetchCarParkAvailibilty();
 		carParkService.fetchCarParkInformation();
 		/* task execution */
 	}
